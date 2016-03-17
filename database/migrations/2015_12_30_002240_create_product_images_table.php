5c799c9d2e8adaec95edd018536c77ab9e161ffa
Alexandre Roch <maycon.alex.rocha@hotmail.com>
2016-03-17 02:56:50 -0300

    consertando migration product_images

--- database/migrations/2015_12_30_002240_create_product_images_table.php
+++ database/migrations/2015_12_30_002240_create_product_images_table.php
@@ -15,7 +15,7 @@ class CreateProductImagesTable extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();
-            $table->foreign('product_id')->references('iid')->on('products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('extension',5);
             $table->timestamps();
         });
